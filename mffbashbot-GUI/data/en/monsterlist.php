06f98fc16f4752bd54811e69a2450d93ca77d3ab
HackerHarry <hackerharry.oss@arcor.de>
2017-01-10 08:16:00 +0100

    completed english translation

--- mffbashbot-GUI/data/en/monsterlist.php
+++ mffbashbot-GUI/data/en/monsterlist.php
@@ -1,4 +1,3 @@
 <?php
-$monsterlist = [1 => "Water bucket", 2 => "Watering can", 3 => "Garden hose", 4 => "Sprinkler", 5 => "Automatic watering", 6 => "Spring water", 7 => "Solar-Watering", 10 => "Light bulb", 11 => "Chandelier", 12 => "Halogen spot light", 13 => "Ultraviolet lamp", 14 => "Daylight lamp", 15 => "Bio lamp", 16=> "LED plant lamp", 20 => "Basic fertiliser", 21 => "Power fertiliser", 22 => "Special fertiliser", 23 => "Growth booster", 24 => "Extreme fertiliser", 25 => "Deluxe fertiliser", 26 => "Bio fertiliser"];
-// loosely translated, needs re-work cuz i didn't have the original game data
+$monsterlist = [1 => "Water bucket", 2 => "Watering can", 3 => "Garden hose", 4 => "Sprinkler", 5 => "Automatized watering", 6 => "Spring water", 7 => "Solar Powered Irrigation", 10 => "Light bulb", 11 => "Chandelier", 12 => "Halogen spot light", 13 => "UV-lamp", 14 => "Daylight lamp", 15 => "Bio lamp", 16=> "LED Growth lamp", 20 => "Simple fertiliser", 21 => "Power fertiliser", 22 => "Special fertiliser", 23 => "Growth booster", 24 => "Extreme fertiliser", 25 => "Deluxe fertiliser", 26 => "Organic fertiliser"];
 ?>
