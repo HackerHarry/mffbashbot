84e238c053cb94dd4e03401a1e052aa4d664379b
HackerHarry <hackerharry.oss@arcor.de>
2024-07-06 20:49:11 +0200

    monster fruit expansion, leaner language files, code cosmetics

--- mffbashbot-GUI/farmdata.php
+++ mffbashbot-GUI/farmdata.php
@@ -11,4 +11,40 @@ $versionavailable = file_get_contents("/tmp/mffbot-version-available.txt");
 ($JSON = file_get_contents("/tmp/forestryproducts-" . $lang . ".txt")) ===  false ? header("Location: index.php") : $forestryproductlist = (json_decode($JSON, true));
 ($JSON = file_get_contents("/tmp/fooddata-" . $username . ".txt")) ===  false ? header("Location: index.php") : $fooddata = (json_decode($JSON, true));
 ($JSON = file_get_contents("/tmp/formulas-" . $lang . ".txt")) ===  false ? header("Location: index.php") : $windmillproductlist = (json_decode($JSON, true));
+
+// monster fruit
+$megafruitObjects = array("water", "light", "fertilize");
+$monsterlist = array();
+if (isset($farmdata["updateblock"]["farmersmarket"]["megafruit"]["objects"])) {
+    for ($objIndex = 0; $objIndex < 3; $objIndex++) {
+        $arr = $farmdata["updateblock"]["farmersmarket"]["megafruit"]["objects"][$megafruitObjects[$objIndex]];
+        for ($count = 0; $count < count($arr); $count++)
+            $monsterlist[$arr[$count]["oid"]] = $arr[$count]["name"];
+        }
+}
+
+// foodworld
+$foodworldproductlist = array();
+if (isset($fooddata["datablock"]["products"])) {
+    $arr = $fooddata["datablock"]["products"];
+    while (current($arr)) {
+        $i = key($arr);
+        $arr2 = $arr[$i]["out"];
+        current($arr2);
+        $objIndex = key($arr2);
+        $foodworldproductlist[$i] = $productlist[$objIndex];
+        next($arr);
+    }
+}
+
+// megafield vehicles
+$megafieldvehicleslist = array();
+if (isset($farmdata["updateblock"]["megafield"]["vehicle_slots"])) {
+    $arr = $farmdata["updateblock"]["megafield"]["vehicle_slots"];
+    while (current($arr)) {
+        $i = key($arr);
+        $megafieldvehicleslist[$i] = $arr[$i]["name"];
+        next($arr);
+    }
+}
 ?>
